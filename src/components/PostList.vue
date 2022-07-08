2aae3f3e7d2074067afe7a2d75b1a0e65b9ce03c
dogogo-wu <dog9zero0@gmail.com>
2022-07-08 14:32:35 +0800

    props, lifecycle hook, add 2 comp

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="post in posts" :key="post.id">
+    <SinglePost :post="post" />
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core';
+import SinglePost from "./SinglePost.vue";
+
+export default {
+  props: ["posts"],
+  components: { SinglePost },
+  setup(props) {
+    console.log(props.posts);
+
+    onMounted(() => console.log("comp mounted"));
+    onUnmounted(() => console.log("comp unmounted"));
+    onUpdated(() => console.log("comp updated"));
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,38 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>Search term - {{search}}</p>
-    <div v-for="name in matchNames" :key="name">
-      {{name}}      
-    </div>
-    <button @click="handleClick">Stop Watching</button>
+    <PostList :posts="posts" v-if="showPost"/>
+    <button @click="showPost = !showPost">Toggle posts</button>
+    <button @click="posts.pop()">Remove a post</button>
   </div>
 </template>
 
 <script>
-import {computed, ref, watch, watchEffect} from 'vue'
+import { ref } from "vue";
+import PostList from "../components/PostList.vue";
 
 export default {
-  name: 'Home',
-  setup(){
-    const search = ref('')
-    const names = ref(['mario', 'sandy', 'cindy', 'angela', 'john', 'zootopia'])
+  name: "Home",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        id: 1,
+        title: "Welcom to the blog",
+        content:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      },
+      {
+        id: 2,
+        title: "Top 5 css tips",
+        content:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      },
+      { id: 3, title: "My profolio", content: "lorem" },
+    ]);
+    const showPost = ref(true);
 
-    const stopWatch = watch(search, ()=>{
-      console.log('watch func run');
-    })
-    const stopEffect = watchEffect(()=>{
-      console.log('watchEffect func run', search.value);
-    })
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    }
-
-    const matchNames = computed(()=>{
-      return names.value.filter((name)=>{
-        return name.includes(search.value)
-      })
-    })
-
-    return {search, names, matchNames, handleClick}
-  }
-}
+    return { posts, showPost };
+  },
+};
 </script>
